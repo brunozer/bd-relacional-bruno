bed08bb01cd545118487093024ea9360374a6941
bruno <bruno.costa218@etec.sp.gov.br>
2024-03-12 19:45:27 -0300

    views (criar as outras duas que ficaram faltando)

--- views_pt1.sql
+++ views_pt1.sql
@@ -3,3 +3,42 @@ select * from tbl_medico;
 
 Select * from listagem_geral_medico
 where cod_medico = 1;
+
+
+
+##LISTAGEM MEDICO/ESPECIALIDADE####
+create view listagem_medico_especialidade as
+select tm.nome_medico, tm.telefone_medico, tm.celular_medico, tm.email_medico,
+te.nome_especialidade
+from tbl_medico as tm;
+
+
+## SALA 
+
+## LISTAGEM AGENDA ##
+create view agenda_aula as
+select
+ta.data_cirurgia, ta.status_cirurgia,
+ts.nro_sala,
+tm.nome_medico, tm.celular_medico,
+tp.nome_paciente, tp.celular_paciente,
+tp.nome_responsavel, tp.telefone_responsavel
+from tbl_agenda as ta
+inner join tbl_sala as ts 
+on ta.cod_sala = ts.cod_sala
+inner join tbl_medico as tm
+on ta.cod_medico = tm.cod_medico
+inner join tbl_paciente as tp
+on ta.cod_paciente= tp.cod_paciente;
+
+
+## Testando as views ####
+SELECT * FROM listagem_medico_especialidade_aula;
+SELECT * FROM listagem_sala_especialidade_aula;
+SELECT * FROM agenda_aula;
+
+
+drop view agenda_aula;
+
+
+
